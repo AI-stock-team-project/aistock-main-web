5693b2d30d61ebfa1c065ba98e071db8cc3cbf72
SH.H <e2xist@gmail.com>
2021-06-09 09:09:37 +0900

    update readme.md

--- mysite.cmd
+++ mysite.cmd
@@ -3,7 +3,6 @@ REM cd c:/projects/mysite
 REM 현재 경로로 이동
 cd /D "%~DP0"
 
-# ---------------------------------- [edit] ---------------------------------- #
 set DJANGO_SETTINGS_MODULE=config.settings.local
-# ---------------------------------------------------------------------------- #
+
 ./venv/scripts/activate
