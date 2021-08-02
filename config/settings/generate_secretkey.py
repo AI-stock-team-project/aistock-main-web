c81e0c4316ee286a0133b33fe26f049d5849f75a
SH.H <e2xist@gmail.com>
2021-08-02 22:33:05 +0900

    update generate_secretkey. 단독 실행 가능하게 변경.

--- config/settings/generate_secretkey.py
+++ config/settings/generate_secretkey.py
@@ -1,2 +1,12 @@
-from django.core.management.utils import get_random_secret_key
+import secrets
+RANDOM_STRING_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+def get_random_secret_key(length=50, allowed_chars=RANDOM_STRING_CHARS):
+    """
+    django/core/management/utils.py 의 get_random_secret_key
+    django/utils/crypto.py 의 get_random_string
+    두 파일을 참조
+    """
+    return ''.join(secrets.choice(allowed_chars) for i in range(length))
+
 print('django-insecure-' + get_random_secret_key())
