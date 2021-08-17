a1240be045c96fe4ed861cd4f48ed393af47acdf
SH.H <e2xist@gmail.com>
2021-08-17 12:33:32 +0900

    accounts/login 추가.

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 import main.views
 import stock.views
 import mypage.views
@@ -27,4 +27,5 @@ urlpatterns = [
     path('test/', main.views.test),
     path('stock/', stock.views.index),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
