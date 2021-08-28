99297cab44be7ae156abe172f8f7c8cf33e45635
SH.H <e2xist@gmail.com>
2021-08-29 05:36:01 +0900

    포트폴리오 화면 구현.

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,17 +8,10 @@ def index(request):
     return render(request, 'portfolio/index.html')
 
 
-def step1(request, optimize_method):
-    page_id = request.GET.get('page', 'choice_years')
-    if page_id == 'choice_years':
-        return render(request, 'portfolio/choice_years.html')
-    elif page_id == 'choice_money':
-        return render(request, 'portfolio/choice_money.html')
-    elif page_id == 'choice_risk':
-        return render(request, 'portfolio/choice_risk.html')
-    elif page_id == 'choice_items':
-        return render(request, 'portfolio/choice_items.html')
+def step1(request, optimize_method='max_sharpe'):
 
-
-    return render(request, 'portfolio/choice_years.html')
+    context = {
+        'optimize_method': optimize_method
+    }
+    return render(request, 'portfolio/choice_years.html', context)
 
