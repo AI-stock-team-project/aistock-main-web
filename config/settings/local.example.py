34552b45e133511ec84e8ca9d874eac4313fd377
SH.H <e2xist@gmail.com>
2021-08-30 15:28:30 +0900

    allowed_hosts 수정.

--- config/settings/local.example.py
+++ config/settings/local.example.py
@@ -9,7 +9,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
