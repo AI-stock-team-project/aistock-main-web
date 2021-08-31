27b5b09952f9a45469bc468a90df12579bd37ffe
SH.H <e2xist@gmail.com>
2021-09-01 04:31:12 +0900

    lstm 그래프 구현.

--- lstm/urls.py
+++ lstm/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('pinned_stock/', views.pinned_stock, name='pinned_stock'),
     path('predict_close_price/', views.predict_close_price_report, name='predict_close_price'),
+    path('predict_close_price_demo/', views.report_demo, name='predict_close_price_demo'),
 ]
